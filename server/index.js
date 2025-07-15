bb756a7714ca20f3ec7d0773407bc636ee2743eb
SOURAV AGARWAL <128616574+Souravagarwal2003@users.noreply.github.com>
2025-07-15 14:26:06 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,23 +24,21 @@ const allowedOrigin = process.env.CLIENT_URL;
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
-
-  // Debug
-  console.log("Incoming origin:", origin);
-
-  if (origin === allowedOrigin) {
+  if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  }
 
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(204); // ⬅️ Use 204 (No Content) for OPTIONS
-    }
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
   }
 
   next();
 });
+
 
 
 
