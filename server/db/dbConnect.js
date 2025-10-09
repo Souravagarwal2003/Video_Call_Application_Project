f51bd3bd384d86138dd219b7dba20903fb63dcce
Harshita <harshita75tiwary@gmail.com>
2025-10-09 15:51:39 +0530

    feat: connect to production database

--- server/db/dbConnect.js
+++ server/db/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const dbConnect=async()=>{
 try {
     await mongoose.connect(process.env.MONGOOSE_CONNECTION),
-    console.log("✅Connected to DataBase");
+    console.log("Connected to DataBase");
 } catch (error) {
     console.log(error);
 }
