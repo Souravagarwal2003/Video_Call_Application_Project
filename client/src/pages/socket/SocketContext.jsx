be888b9f612d4b0554ccd4de250668131741245b
Harshita <harshita75tiwary@gmail.com>
2025-10-09 16:10:04 +0530

    Updated socket.io.js

--- client/src/pages/socket/SocketContext.jsx
+++ client/src/pages/socket/SocketContext.jsx
@@ -4,7 +4,7 @@ let socket;
 
 const getSocket = () => {
     if (!socket) {
-        socket = io(import.meta.env.VITE_API_SOCKET_URL)
+        socket = io(import.meta.env.VITE_BACKEND_URL)
     }
     return socket;
 }
